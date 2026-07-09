4fd6dab2a72e63162aeeaef5d5252975e7beebde
Fernando J. Chaure <5598671+ferchaure@users.noreply.github.com>
2026-07-08 23:25:16 -0300

    upload publish.yml

--- setup.py
+++ setup.py
@@ -2,6 +2,15 @@ import sys
 from setuptools import setup, Extension
 import numpy as np
 
+if sys.platform == 'win32':
+    # MSVC: /TC = compile as C, /O2 = optimize; -std=c99 is not valid
+    compile_args = ['/O2', '/TC']
+    link_args = []
+else:
+    # GCC / Clang (Linux, macOS)
+    compile_args = ['-std=c99', '-O2']
+    link_args = ['-lm']
+
 ext = Extension(
     'eiko_skelfm._msfm',
     sources=[
@@ -11,8 +20,8 @@ ext = Extension(
         'src/msfm3d.c',
     ],
     include_dirs=[np.get_include(), 'src/'],
-    extra_compile_args=['-std=c99', '-O2'],
-    extra_link_args=['-lm'] if sys.platform != 'win32' else [],
+    extra_compile_args=compile_args,
+    extra_link_args=link_args,
 )
 
 setup(ext_modules=[ext])
